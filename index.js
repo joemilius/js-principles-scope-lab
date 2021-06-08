0f03f264bd4ffe1188d040751be6d111fb748351
JoeMilius <joemilius@gmail.com>
2021-06-08 11:12:37 -0600

    commit tree 4fed79d755ae0dabd1f067a642801f5cd7e13539

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+var customerName = 2;
+
+/*
+function upperCaseCustomerName() {
+   var upperCase = customerName.toUppercase();
+    return upperCase;
+}
+
+upperCaseCustomerName();
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+setBestCustomer();
+
+function overwriteBestCustomer() {
+    var bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+overwriteBestCustomer();
+
+const leastFavoriteCustomer = 'Jojo Momo';
+
+function changeLeastFavoriteCustomer(leastFavoriteCustomer) {
+    const leastFavoriteCustomer = 'Bobby Fischer';
+    return leastFavoriteCustomer;
+}
+
+changeLeastFavoriteCustomer();
