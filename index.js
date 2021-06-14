59b0edd7ed8eb68a5ad210d20e54e3daf6af9013
JoeMilius <joemilius@gmail.com>
2021-06-14 17:17:30 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'Bobby Fischer';
+    return leastFavoriteCustomer = 'Jojo Momo';
+}
